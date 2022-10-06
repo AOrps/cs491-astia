bd0d7fe5ac409f6f017330bb3b546878ea1fbc3a
Naveed <tan7@njit.edu>
2022-10-06 19:31:46 -0400

    tasin-astia

---
+++ Astial-final-version-master/database.py
@@ -0,0 +1,25 @@
+import os
+from deta import Deta
+from dotenv import load_dotenv
+import streamlit as st
+
+
+DETA_KEY = st.secrets["key"]
+
+deta = Deta(DETA_KEY)
+
+db = deta.Base("user_db")
+
+def insert_user(username, name, password):
+    return db.put({"key": username, "name": name, "password": password})
+#insert_user("naveed", "tasin naveed", "xxx")
+
+def fetch_all_users():
+    res = db.fetch()
+    return res.items
+
+
+
+def get_user(username):
+    return db.get(username)
+
